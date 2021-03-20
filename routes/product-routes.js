7a1242a1b2851189bc24d12b2fbdfee0aeb8e5c7
Ben Hopkins <bjhops17@gmail.com>
2021-03-20 10:58:23 -0700

    second repository attempt

---
+++ routes/product-routes.js
@@ -0,0 +1,51 @@
+const products = require("../seed")
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/api/products/:category", async (req, res) =>{
+    let data = await db.Products.findAll({
+        where: {
+            category: req.params.category
+        }
+    }).catch(err => res.status(500))
+    res.json(data)
+    
+})
+
+router.get("/api/products/subCat/:category", async (req, res) =>{
+    let data = await db.Products.findAll({
+        where: {
+            subCategory: req.params.category
+        }
+    }).catch(err => res.status(500))
+    res.json(data)
+    
+})
+router.get("/api/featured", async (req, res) =>{
+    let data = await db.Products.findAll({
+        where: {
+            featured: true
+        }
+    }).catch(err => res.status(500))
+    res.json(data)
+    
+})
+
+router.get("/api/products/item/:id", async (req, res) =>{
+    let data = await db.Products.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).catch(err => res.status(500))
+    res.json(data)
+    
+})
+router.post("/", async (req, res) =>{
+    db.Products.bulkCreate(products)
+    res.status(200)
+    
+})
+
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,44 @@
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require("./models");
+// Sets up the Express App
+require("dotenv").config()
+
+var PORT = process.env.PORT || 8080;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var corsOptions = {
+  origin: 'https://bjh-hop-estore.herokuapp.com'
+}
+// corsOptions
+app.use(cors(corsOptions));
+// Static directory
+app.use(express.static("public"));
+/////////////////////////////////
+const adminroutes = require("./routes/admin-routes.js")
+const cartroutes = require("./routes/cart-routes.js")
+// const customerroutes = require("./routes/customer-routes.js")
+const productroutes = require("./routes/product-routes.js")
+
+// Routes
+// =============================================================
+app.use(adminroutes)
+app.use(cartroutes)
+app.use(productroutes)
+
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+
+// Change force: to true if it's cool for the site to remove database items.
+db.sequelize.sync({ force: false}).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT http://localhost:" + PORT);
+  });
+});
