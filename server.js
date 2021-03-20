a32362ca1377443b69ae5a8f8dcc6a74b8f2b13c
Ben Hopkins <bjhops17@gmail.com>
2021-03-20 11:29:35 -0700

    test

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var corsOptions = {
   origin: 'https://bjh-hop-estore.herokuapp.com'
 }
 // corsOptions
-app.use(cors(corsOptions.origin));
+app.use(cors(corsOptions));
 // Static directory
 app.use(express.static("public"));
 /////////////////////////////////
